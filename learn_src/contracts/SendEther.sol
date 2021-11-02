0b50a9010d40f3827be2f149a2a621e38a0e9b6f
Sahil Vasava <sahilvasava@gmail.com>
2021-11-02 20:09:51 +0530

    added a boileplate  code for compilation

--- learn_src/contracts/SendEther.sol
+++ learn_src/contracts/SendEther.sol
@@ -44,7 +44,7 @@ contract SendEther {
 
     function sendViaCall(address payable _to) public payable {
         // write logic to send Ether to _to via Call
-         
-        require(sent, "Failed to send Ether");
+
+        
     }
 }
